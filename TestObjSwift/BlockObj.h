81cb1c229a692ea422573560cf2d7d645c8b8a35
Vien Vu <lamvienbk@gmail.com>
2015-11-16 18:26:27 +0700

    first commit

---
+++ TestObjSwift/BlockObj.h
@@ -0,0 +1,13 @@
+//
+//  BlockObj.h
+//  TestObjSwift
+//
+//  Created by Vien Vu  on 10/26/15.
+//  Copyright © 2015 VienVu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockObj : NSObject
+
+@end
---
+++ TestObjSwift/BlockObj.m
@@ -0,0 +1,13 @@
+//
+//  BlockObj.m
+//  TestObjSwift
+//
+//  Created by Vien Vu  on 10/26/15.
+//  Copyright © 2015 VienVu. All rights reserved.
+//
+
+#import "BlockObj.h"
+
+@implementation BlockObj
+
+@end
